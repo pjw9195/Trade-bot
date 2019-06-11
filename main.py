281507ae2c99b5a8543da360096cf66b250bce3a
Park <pjw9195@naver.com>
2019-06-11 17:08:00 +0900

    중간발표 최종 수정

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-from twitterstreaming import twitterStream
-
-#샀을때는 더이상 사지않도록 하는 변수
+from twitterstreaming import *
 twitterStream.filter(follow=["295218901", "3367334171", "28785486", "742143"])
--- twitterstreaming.py
+++ twitterstreaming.py
@@ -1,8 +1,22 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
-from trade import *
+import re
+from key import *
+import pyupbit
+import websocket
 import time
+import json
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
+access_key = ac_key
+secret_key = se_key
+upbit = pyupbit.Upbit(access_key, secret_key)
+import nltk
+from nltk.corpus import stopwords
 from train import model
 from key import *
 
@@ -25,13 +39,13 @@ class listener(StreamListener):
         if from_creator(status):
             try:
                 # Prints out the tweet
+                parse = re.sub('https?://\S+','',status.text)
+                parse = re.sub('#\S+','',parse)
                 if model.classify(status.text) > 0.7:
+                    print(status.text, model.classify(parse))
                     buy()
-                    print(status.text)
-                    def swi(swi):
-                        swi =0
-                        return swi
 
+                print(status.text, model.classify(parse))
                 time.sleep(2)
                 return True
             except BaseException as e:
@@ -47,3 +61,68 @@ auth.set_access_token(access_token, access_token_secret)
 twitterStream = Stream(auth, listener())
 
 
+def mybalnace():
+    # 내 돈
+    print(upbit.get_balances())
+    # print((upbit.get_balances())[0][0]['currency'] +' : ' + (upbit.get_balances())[0][0]['balance'] )
+def find():
+
+    def on_message(ws, message):
+
+        get_message = json.loads(message.decode('utf-8'))
+        print(get_message)
+        global msg_ask
+        msg_ask = get_message['orderbook_units'][5]['ask_price']
+        global msg_bid
+        msg_bid = get_message['orderbook_units'][3]['bid_price']
+        global msg_cur
+        msg_cur = get_message['orderbook_units'][0]['bid_price']
+        # 한번만받고 종료.
+        ws.close()
+
+
+    def on_error(ws, error):
+        print(error)
+
+    def on_close(ws):
+        print("close")
+
+
+    def on_open(ws):
+        def run(*args):
+            ws.send('[{"ticket":"test"},{"type":"orderbook","codes":["KRW-BTC"]}]')
+
+            time.sleep(0.1)
+
+        thread.start_new_thread(run, ())
+
+    ws = websocket.WebSocketApp("wss://api.upbit.com/websocket/v1",
+                                  on_message = on_message,
+                                  on_error = on_error,
+                                  on_close = on_close)
+
+    ws.on_open = on_open
+    ws.run_forever()
+
+def buy():
+    find()
+    getbalance = upbit.get_balances()
+    KRW = getbalance[0][0]['balance']
+    num = (float(KRW)*0.9/float(msg_ask))
+    ret = upbit.buy_limit_order("KRW-BTC", msg_ask, num );
+    print(ret)
+    cur = msg_cur
+    while True:
+        if cur*0.95 > msg_cur:
+            sell()
+            break
+        elif cur*1.05 <msg_cur:
+            sell()
+            break
+        time.sleep(2)
+
+
+def sell():
+    find()
+    ret = upbit.sell_limit_order("KRW-BTC", msg_bid, (upbit.get_balances())[0][0]['balance']);
+    print(ret)
